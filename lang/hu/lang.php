e3a7d53c606987b0d77f72cfad6ea85f143100fe
DanSketic <dansketic@gmail.com>
2021-05-02 00:43:38 +0200

    Add new HU lang

---
+++ lang/hu/lang.php
@@ -0,0 +1,86 @@
+<?php
+
+return [
+    'online'                => 'Online',
+    'login'                 => 'Belépés',
+    'logout'                => 'Kilépés',
+    'frontend'              => 'Frontend',
+    'setting'               => 'Beállítás',
+    'name'                  => 'Név',
+    'username'              => 'Felhasználónév',
+    'password'              => 'Jelszó',
+    'password_confirmation' => 'Jelszó megerősítése',
+    'remember_me'           => 'Emlékezz rám',
+    'user_setting'          => 'Felhasználói beállítás',
+    'avatar'                => 'Avatar',
+    'member_since'          => 'Tag',
+
+    'list'                  => 'Lista',
+    'new'                   => 'Új',
+    'create'                => 'Létrehoz',
+    'delete'                => 'Töröl',
+    'remove'                => 'Eltávolít',
+    'edit'                  => 'Szerkesztés',
+    'view'                  => 'Nézet',
+    'browse'                => 'Böngész',
+    'reset'                 => 'Visszaállítás',
+    'export'                => 'export',
+    'batch_delete'          => 'Kötegelt törlés',
+    'save'                  => 'Mentés',
+    'refresh'               => 'Frissítés',
+    'order'                 => 'Rendelés',
+    'expand'                => 'Kinyit',
+    'collapse'              => 'Összezár',
+    'filter'                => 'Szűrő',
+    'close'                 => 'Bezárás',
+    'show'                  => 'Mutat',
+    'entries'               => 'bejegyzés',
+    'captcha'               => 'Captcha',
+
+    'action'                => 'Művelet',
+    'title'                 => 'Cím',
+    'description'           => 'Leírás',
+    'back'                  => 'Vissza',
+    'back_to_list'          => 'Vissza a listához',
+    'submit'                => 'Beküldés',
+    'menu'                  => 'Menü',
+    'input'                 => 'Bemenet',
+    'succeeded'             => 'Sikerült',
+    'failed'                => 'Sikertelen',
+    'delete_confirm'        => 'Biztos, hogy törli ezt az elemet?',
+    'delete_succeeded'      => 'Törlés sikerült!',
+    'delete_failed'         => 'Törlés sikertelen!',
+    'update_succeeded'      => 'A frissítés sikerült!',
+    'save_succeeded'        => 'A mentés sikerült!',
+    'refresh_succeeded'     => 'Frissítés sikerült!',
+    'login_successful'      => 'Sikeres belépés',
+
+    'choose'                => 'Választ',
+    'choose_file'           => 'Válaszd ki a fájlt',
+    'choose_image'          => 'Válassza ki a képet',
+
+    'more'                  => 'Több',
+    'deny'                  => 'Hozzáférés megtagadva',
+
+    'administrator'         => 'Adminisztrátor',
+    'roles'                 => 'Szerepek',
+    'permissions'           => 'Engedélyek',
+    'slug'                  => 'Slug',
+
+    'created_at'            => 'Létrehozott',
+    'updated_at'            => 'Frissítve',
+
+    'alert'                 => 'Hiba',
+
+    'parent_id'             => 'Szülő',
+    'icon'                  => 'Ikon',
+    'uri'                   => 'URI',
+
+    'operation_log'         => 'Műveleti napló',
+    'parent_select_error'   => 'Szülőválasztás hiba',
+
+    'pagination'            => [
+        'range' => 'Megjelenítve :first - :last, összesen :total bejegyzés',
+    ],
+
+];
--- src/Backport.php
+++ src/Backport.php
@@ -24,7 +24,7 @@ class Backport
      *
      * @var string
      */
-    const VERSION = '1.0.3';
+    const VERSION = '1.0.4';
 
     /**
      * @var Navbar
